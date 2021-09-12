e1413fd9a749852eff1907316d1dac5ff12b1bf6
Harmony Moyo <harmonymoyo17@gmail.com>
2021-09-12 07:33:48 +0100

    do not recieve your own sent message
    The sent message is recieved by everyone in the room except for the sender
    
    [Ticket: 73ecd13dade725b60afa4eea2a0acd28cb868f02ec801caaf8fc2f50400174c7]

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -1,11 +1,59 @@
 import { dom } from './dom.mjs';
 
+class ChatApp {
+    constructor(socket = io()) {
+        this.socket = socket;
+    }
 
-function makeMessage(message, type) {
-    let messageElement = dom.makeHTMLElement('li', {class: `message ${type === 'recieved' ? 'reciepient': 'sent'}`});
-    messageElement.innerText = message;
-    return messageElement;
+    send(message) {
+        this.socket.emit('message', message);
+    }
+
+    recieve(callback) {
+        this.socket.on('message', msg => {
+            callback(msg);
+        })
+    }
+
+    disconnect() {
+        console.log('disconnecting from server');
+        this.socket.emit('disconnect');
+    }
+    onDisconnect(callback) {
+        this.socket.on('disconnect', msg => {
+            callback(msg);
+        })
+    }
 }
+
+class ChatUi {
+    static MESSAGE_SENT = 'sent';
+    static MESSAGE_RECIEVED = 'recieved';
+    static DISCONNECTED = 'disconnected';
+    static CONNECTED = 'connected';
+
+    constructor() {
+
+    }
+    static isValidType(type) {
+        let valid_types = [ ChatUi.MESSAGE_RECIEVED, ChatUi.MESSAGE_SENT, ChatUi.CONNECTED, ChatUi.DISCONNECTED ];
+        return valid_types.find( t => t === type);
+    }
+
+    chatMessage(message, type=ChatUi.MESSAGE_SENT) {
+        if(!ChatUi.isValidType(type)) {
+            throw new Error('Failed to create chat message invalid message type: '+ type);
+        }
+        let chatElement = dom.makeHTMLElement('li', 
+        {class: `message ${type}`});
+        chatElement.innerText = message;
+        return chatElement;
+    }
+}
+
+
+
+
 class App {
     constructor() {
         if (document.addEventListener) {
@@ -16,34 +64,29 @@ class App {
     }
 
     main() {
-        const socket = io();
-        let send_button = dom.query(".send-message-btn");
-        let message_input = dom.query(".message-input");
-        let messages = dom.query(".messages-timeline");
-        socket.on('user-connected', msg => {
-            console.log('user connected')
-            let connectionMessage = dom.makeHTMLElement('li', {
-                class: 'message user-connected'
-            });
-            connectionMessage.innerText = 'new user connnected';
-            messages.appendChild(connectionMessage);
-        })
+        let chatApp = new ChatApp();
+        let chatUi = new ChatUi();
 
-        socket.on('user-disconnected', msg => {
-            console.log('user connected')
-            let connectionMessage = dom.makeHTMLElement('li', {
-                class: 'message user-connected'
-            });
-            connectionMessage.innerText = 'new user connnected';
-            messages.appendChild(connectionMessage);
-        })
-        
-        send_button.addEventListener('click', () => {
-            messages.appendChild(makeMessage(message_input.value, 'sent'));
-            socket.emit('message', message_input.value);
-            message_input.value = "";
+        let messages = dom.query('.messages-timeline');
+        let message_input = dom.query('.message-input');
+        let send_message_btn = dom.query('.send-message-btn');
 
+
+        chatApp.recieve(message => {
+            let uiMessage = chatUi.chatMessage(message, ChatUi.MESSAGE_RECIEVED);
+            messages.appendChild(uiMessage);
         });
+        send_message_btn.addEventListener('click', () => {
+            let message = message_input.value;
+            if(message.length < 1)
+                return;
+            message_input.value = "";
+            chatApp.send(message);
+            let uiMessage = chatUi.chatMessage(message, ChatUi.MESSAGE_SENT);
+            messages.appendChild(uiMessage);            
+        });
+
+
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -8,18 +8,12 @@ const port = 3000;
 server.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
 
 app.use('/', express.static('public/'));
-// app.get('/', (req, res) => {
-//     res.sendFile({root: __dirname,},'public/index.html');
-// })
+
 
 io.on('connection', socket => {
-    console.log(`user connected`);
-    socket.emit('user-connected', 'user connected');
-    socket.emit('message', {Harmony: 'How are you?'});
-
-
     socket.on('message', message => {
-        console.log(`message: ${message}`)
-        socket.emit('message', message)
+        console.log(message);
+        socket.broadcast.emit('message', message);
     })
+
 })
